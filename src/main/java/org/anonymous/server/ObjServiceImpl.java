b8239a9c6f3fca7895a5e73449880adab369bdcf
somnath67643 <somnath67643@gmail.com>
2020-09-28 17:36:03 +0530

    Review

--- src/main/java/org/anonymous/server/ObjServiceImpl.java
+++ src/main/java/org/anonymous/server/ObjServiceImpl.java
@@ -1,15 +1,72 @@
 package org.anonymous.server;
 
-import org.anonymous.grpc.ObjServiceGrpc.ObjServiceImplBase;
-import org.anonymous.grpc.CmdConnect;
 import io.grpc.stub.StreamObserver;
-import org.anonymous.grpc.CmdConnectResponse;
+import org.anonymous.grpc.*;
+import org.anonymous.grpc.ObjServiceGrpc.ObjServiceImplBase;
+import org.anonymous.module.ObjectRepository;
+import org.anonymous.util.TimeKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ObjServiceImpl extends ObjServiceImplBase {
 
-    public void connect(CmdConnect request, StreamObserver<CmdConnectResponse> responseObserver) {
+    private static ObjectRepository objectRepositiory;
 
-        System.out.println("yo......");
+    private static Logger LOGGER = LoggerFactory.getLogger(ObjServiceImpl.class);
+
+    ObjServiceImpl(ObjectRepository objectRepositiory) {
+        this.objectRepositiory = objectRepositiory;
     }
 
+    @Override
+    public void lookupByName(CmdLookupByName request, StreamObserver<CmdLookupByNameResponse> responseObserver) {
+        final TimeKeeper timekeeper = new TimeKeeper();
+
+        try {
+            int limit = request.getCount();
+            String prefix = "";
+            if (request.getSecurityNamePrefix() != null) {
+                prefix = request.getSecurityNamePrefix();
+            }
+
+            CmdLookupByNameResponse.Builder responseBuilder = CmdLookupByNameResponse.newBuilder();
+            objectRepositiory.lookup(prefix, limit, timekeeper).stream().forEach(key -> responseBuilder.addSecurityNames(key));
+            responseObserver.onNext(responseBuilder.build());
+            responseObserver.onCompleted();
+
+        } catch (Exception e) {
+            LOGGER.info("Caught Exception", e);
+        }
+    }
+
+    @Override
+    public void lookupByNameStream(CmdLookupByName request, StreamObserver<CmdLookupByNameResponseStream> responseObserver) {
+        //super.lookupByNameStream(request, responseObserver);
+    }
+
+    @Override
+    public void lookupByType(CmdNameLookupByType request, StreamObserver<CmdNameLookupByTypeResponse> responseObserver) {
+        final TimeKeeper timekeeper = new TimeKeeper();
+        try {
+            int limit = request.getCount();
+            String prefix = "";
+            if (request.getSecurityNamePrefix() != null) {
+                prefix = request.getSecurityNamePrefix();
+            }
+            int typeid = request.getGetType().getNumber();
+
+            CmdNameLookupByTypeResponse.Builder responseBuilder = CmdNameLookupByTypeResponse.newBuilder();
+            objectRepositiory.lookupById(prefix, typeid, limit, timekeeper).stream().forEach(key -> responseBuilder.addSecurityNames(key));
+            responseObserver.onNext(responseBuilder.build());
+            responseObserver.onCompleted();
+
+        } catch (Exception e) {
+            LOGGER.info("Caught Exception", e);
+        }
+    }
+
+    @Override
+    public void lookupByTypeStream(CmdNameLookupByType request, StreamObserver<CmdNameLookupByTypeResponseStream> responseObserver) {
+        // super.lookupByTypeStream(request, responseObserver);
+    }
 }
