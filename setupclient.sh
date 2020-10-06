231840163166c81f56668a53c203502b0186180c
somnath67643 <somnath67643@gmail.com>
2020-10-06 22:46:53 +0530

    Lookup

--- setupclient.sh
+++ setupclient.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+echo "Start Client"
+
 git clone https://github.com/somnath67643/aurora-sizing.git
 
-cd aurora-sizing/clientgo
+cd /home/ec2-user/environment/aurora-sizing/clientgo
 
 make clean
 make
 
 ./testclient
+
+echo "Run Complete"
 
