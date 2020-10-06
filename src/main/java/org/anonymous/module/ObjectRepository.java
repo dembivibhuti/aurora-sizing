312e0d145737b0081cd34b13902ba156ebe7b8f1
Sushmitha98 <sushmitharajeswari98@gmail.com>
2020-10-06 12:29:53 +0530

    made changes to ObjectRepository to getmany

--- src/main/java/org/anonymous/module/ObjectRepository.java
+++ src/main/java/org/anonymous/module/ObjectRepository.java
@@ -543,7 +543,6 @@ public class ObjectRepository implements AutoCloseable {
     public List<ByteString> getManyMemByName(ProtocolStringList securityNameList, TimeKeeper getManyTimeKeeper) {
         List<ByteString> secMem = new ArrayList<>();
         String sql = String.format(GET_MANY_MEM_RECORDS, String.join(",", Collections.nCopies(securityNameList.size(), "?")));
-
         try (Connection connection = roConnectionProvider.getConnection();
              PreparedStatement getManyStmt = connection.prepareStatement(sql)) {
             long spanId = getManyTimeKeeper.start();
@@ -565,7 +564,6 @@ public class ObjectRepository implements AutoCloseable {
     public List<CmdGetManyByNameExtResponse.ResponseMessage> getManySDBByName(ProtocolStringList securityNameList, TimeKeeper getManyTimeKeeper) {
         List<CmdGetManyByNameExtResponse.ResponseMessage> responseMessages = new ArrayList<>();
         String sql = String.format(GET_MANY_RECORDS, String.join(",", Collections.nCopies(securityNameList.size(), "?")));
-
         try (Connection connection = roConnectionProvider.getConnection();
              PreparedStatement getManyStmt = connection.prepareStatement(sql)) {
             long spanId = getManyTimeKeeper.start();
@@ -605,7 +603,6 @@ public class ObjectRepository implements AutoCloseable {
     public List<CmdGetManyByNameExtResponseStream> getManySDBByNameStream(ProtocolStringList securityNameList, TimeKeeper getManyTimeKeeper) {
         List<CmdGetManyByNameExtResponseStream> responseMessages = new ArrayList<>();
         String sql = String.format(GET_MANY_RECORDS, String.join(",", Collections.nCopies(securityNameList.size(), "?")));
-
         try (Connection connection = roConnectionProvider.getConnection();
              PreparedStatement getManyStmt = connection.prepareStatement(sql)) {
             long spanId = getManyTimeKeeper.start();
