9fdbd637136bde2832a7433d442cbd5b854e44fd
somnath67643 <somnath67643@gmail.com>
2020-10-07 02:22:12 +0530

    client install

--- setupclient.sh
+++ setupclient.sh
@@ -2,9 +2,17 @@
 
 echo "Start Client"
 
+sudo yum update -y
+sudo yum install git -y
+git version
+
+wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.2.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' > ~/.profile
+
 git clone https://github.com/somnath67643/aurora-sizing.git
 
-cd /home/ec2-user/environment/aurora-sizing/clientgo
+cd ~/aurora-sizing/clientgo
 
 make clean
 make
