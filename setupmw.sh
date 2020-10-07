f0fcc51bc7967fbe6e28f6021053af649e7e63a9
somnath67643 <somnath67643@gmail.com>
2020-10-08 02:41:24 +0530

    install certs

--- setupmw.sh
+++ setupmw.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+rm -rf ~/aurora-sizing
+
+sudo yum update -y
+sudo yum install git -y
+git version
+
 git clone https://github.com/somnath67643/aurora-sizing.git
 
 sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
@@ -14,9 +20,14 @@ sudo /usr/sbin/alternatives --set javac /usr/lib/jvm/java-1.8.0-openjdk.x86_64/b
 
 cd ~/aurora-sizing
 
+wget https://s3.amazonaws.com/rds-downloads/rds-ca-2019-root.pem
+openssl x509 -in rds-ca-2019-root.pem -out rds-ca-2019-root.pem.der -outform der
+keytool -keystore aurora-postgresql.jks -alias aurora-postgresql -import -file rds-ca-2019-root.pem.der -storepass aurora-postgresql -noprompt
+rm rds-ca-2019-root.pem.der
+
 mvn clean formatter:format compile package
 
-java \
+nohup java \
 -server \
 -Xms8g \
 -Xmx8g \
@@ -29,7 +40,9 @@ java \
 -DdataSource.portNumber=5432 \
 -DdataSource.roserverName=database-1.cluster-ro-cpw6mwbci5yo.us-east-1.rds.amazonaws.com \
 -DdataSource.rwserverName=database-1.cluster-cpw6mwbci5yo.us-east-1.rds.amazonaws.com \
+-Djavax.net.ssl.trustStore=aurora-postgresql.jks \
+-Djavax.net.ssl.trustStorePassword=aurora-postgresql \
 -Dport=8080 \
--jar ~/aurora-sizing/target/aurora-sizing-1.0-SNAPSHOT.jar
+-jar ~/aurora-sizing/target/aurora-sizing-1.0-SNAPSHOT.jar &
 
 
