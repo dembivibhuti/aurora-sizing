23e1498b17976547bc3681a40ad297e5de49b5ec
Muhammad Falak R Wani <falakreyaz@gmail.com>
2020-10-09 17:44:01 +0530

    clientgo: server: reduce duplicated code
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- clientgo/server/main.go
+++ clientgo/server/main.go
@@ -26,12 +26,6 @@ func main() {
 	grpcServer := grpc.NewServer(opts...)
 	objSrvr := &objServer{}
 	pb.RegisterObjServiceServer(grpcServer, objSrvr)
-	//Connect:                      connect,
-	//GetObject:                    getObject,
-	//LookupByNameStream:           lookupByNameStream,
-	//GetObjectManyByNameStream:    getObjectManyByNameStream,
-	//GetObjectManyByNameExtStream: getObjectManyByNameExtStream,
-	//})
 	grpcServer.Serve(lis)
 }
 
@@ -39,14 +33,8 @@ type objServer struct {
 	pb.UnimplementedObjServiceServer
 }
 
-func (o *objServer) Connect(_ context.Context, in *pb.CmdConnect) (*pb.CmdConnectResponse, error) {
-	log.Println(in.AppName)
-	resp := &pb.CmdConnectResponse{
-		MsgSize:     111,
-		VerAndRev:   222,
-		FeatureFlag: 333,
-	}
-	return resp, nil
+func (o *objServer) Connect(ctx context.Context, in *pb.CmdConnect) (*pb.CmdConnectResponse, error) {
+	return connect(ctx, in)
 }
 
 func (o *objServer) ConnectExt(_ context.Context, _ *pb.CmdConnectExt) (*pb.CmdConnectExtResponse, error) {
@@ -58,30 +46,19 @@ func (o *objServer) LookupByName(_ context.Context, _ *pb.CmdLookupByName) (*pb.
 }
 
 func (o *objServer) LookupByNameStream(in *pb.CmdLookupByName, stream pb.ObjService_LookupByNameStreamServer) error {
-	for i := 0; i < int(in.Count); i++ {
-		resp := &pb.CmdLookupByNameResponseStream{
-			SecurityName: fmt.Sprintf("%s_%d", in.SecurityNamePrefix, i),
-		}
-		if err := stream.Send(resp); err != nil {
-			return err
-		}
-	}
-	return nil
+	return lookupByNameStream(in, stream)
 }
 
 func (o *objServer) LookupByType(_ context.Context, _ *pb.CmdNameLookupByType) (*pb.CmdNameLookupByTypeResponse, error) {
 	panic("not implemented") // TODO: Implement
 }
 
-func (o *objServer) LookupByTypeStream(_ *pb.CmdNameLookupByType, _ pb.ObjService_LookupByTypeStreamServer) error {
-	panic("not implemented") // TODO: Implement
+func (o *objServer) LookupByTypeStream(in *pb.CmdNameLookupByType, stream pb.ObjService_LookupByTypeStreamServer) error {
+	return lookupByTypeStream(in, stream)
 }
 
-func (o *objServer) GetObject(_ context.Context, in *pb.CmdGetByName) (*pb.CmdGetByNameResponse, error) {
-	resp := &pb.CmdGetByNameResponse{
-		Response: &pb.CmdGetByNameResponse_Security{Security: getRandomBytes(rand.Intn(32 * 1024))},
-	}
-	return resp, nil
+func (o *objServer) GetObject(ctx context.Context, in *pb.CmdGetByName) (*pb.CmdGetByNameResponse, error) {
+	return getObject(ctx, in)
 }
 
 func (o *objServer) GetObjectExt(_ context.Context, _ *pb.CmdGetByNameExt) (*pb.CmdGetByNameExtResponse, error) {
@@ -93,22 +70,7 @@ func (o *objServer) GetObjectManyByName(_ context.Context, _ *pb.CmdGetManyByNam
 }
 
 func (o *objServer) GetObjectManyByNameStream(in *pb.CmdGetManyByName, stream pb.ObjService_GetObjectManyByNameStreamServer) error {
-	for _, v := range in.SecurityNames {
-		imsg := &pb.CmdGetManyByNameResponseStream_MsgOnSuccess{
-			Mem: getRandomBytes(rand.Intn(1024 * 64)),
-		}
-		msg := &pb.CmdGetManyByNameResponseStream_MsgOnSuccess_{
-			MsgOnSuccess: imsg,
-		}
-		resp := &pb.CmdGetManyByNameResponseStream{
-			MessageResponse: msg,
-		}
-		log.Println("Sending Response for: ", v)
-		if err := stream.Send(resp); err != nil {
-			return err
-		}
-	}
-	return nil
+	return getObjectManyByNameStream(in, stream)
 }
 
 func (o *objServer) GetObjectManyByNameExt(_ context.Context, _ *pb.CmdGetManyByNameExt) (*pb.CmdGetManyByNameExtResponse, error) {
@@ -116,24 +78,7 @@ func (o *objServer) GetObjectManyByNameExt(_ context.Context, _ *pb.CmdGetManyBy
 }
 
 func (o *objServer) GetObjectManyByNameExtStream(in *pb.CmdGetManyByNameExt, stream pb.ObjService_GetObjectManyByNameExtStreamServer) error {
-	for _, v := range in.SecurityNames {
-		imsg := &pb.CmdGetManyByNameExtResponseStream_MsgOnSuccess{
-			HasSucceeded: true,
-			Metadata:     nil,
-			Mem:          getRandomBytes(rand.Intn(64 * 1024)),
-		}
-		msg := &pb.CmdGetManyByNameExtResponseStream_MsgOnSuccess_{
-			MsgOnSuccess: imsg,
-		}
-		log.Println("Sending Response (EXT) for: ", v)
-		resp := &pb.CmdGetManyByNameExtResponseStream{
-			RequestResponse: msg,
-		}
-		if err := stream.Send(resp); err != nil {
-			return err
-		}
-	}
-	return nil
+	return getObjectManyByNameExtStream(in, stream)
 }
 
 func (o *objServer) ChangeInitData(_ context.Context, _ *pb.CmdChangeInitData) (*pb.CmdChangeInitDataResponse, error) {
