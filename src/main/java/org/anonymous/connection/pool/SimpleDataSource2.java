3a45f2ddd48f3a342b37b82a3dfcf6ee729f4501
somnath67643 <somnath67643@gmail.com>
2020-11-10 16:28:08 +0530

    SimpleJDBCConnectionPool

--- src/main/java/org/anonymous/connection/pool/SimpleDataSource2.java
+++ src/main/java/org/anonymous/connection/pool/SimpleDataSource2.java
@@ -223,9 +223,10 @@ public class SimpleDataSource2 implements AutoCloseable {
         Connection conn;
         while ((conn = recycledConnections.poll()) == null) {
             if (System.currentTimeMillis() > timeoutTime) {
-                LOGGER.error("Failed to retrieve connection from Pool {} - Time Waited = {}" , poolName, maxTimeout );
-                throw new TimeoutException("Timeout while waiting for a valid database connection from Pool. maximumPoolSize = " + maximumPoolSize
-                        + " Active Connections = " + activeConnections.get() + " Idle Connections = " + recycledConnections.size());
+                LOGGER.warn("Failed to retrieve connection from Pool {} - Time Waited = {} | maximumPoolSize = {} | activeConnections = {}", poolName, maxTimeout, activeConnections.get());
+                /*throw new TimeoutException("Timeout while waiting for a valid database connection from Pool. maximumPoolSize = " + maximumPoolSize
+                        + " Active Connections = " + activeConnections.get() + " Idle Connections = " + recycledConnections.size());*/
+                timeoutTime = System.currentTimeMillis() + maxTimeout;
             }
         }
         activeConnections.incrementAndGet();
