97b60427f0ba32dcd00f1f8af60a126ff239b866
somnath67643 <somnath67643@gmail.com>
2020-11-26 02:52:07 +0530

    From CSV

--- src/main/java/org/anonymous/module/ObjectRepository.java
+++ src/main/java/org/anonymous/module/ObjectRepository.java
@@ -27,7 +27,7 @@ import static org.anonymous.sql.Store.*;
 
 public class ObjectRepository implements AutoCloseable {
 
-    private static final ExecutorService executorService = Executors.newFixedThreadPool(3600);
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(1000);
     private static final ExecutorService dbOpsExecutorService = Executors.newCachedThreadPool();
     private static final Logger LOGGER = LoggerFactory.getLogger(ObjectRepository.class);
     private static final Gauge obtainDBConnFromPool = Gauge.build().name("get_db_conn_from_pool").help("Get DB Connection from Pool").labelNames("db_ops").register();
