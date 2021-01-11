932b11969dbeae90f56c3b34cc6bfe837edf3a13
somnath67643 <somnath67643@gmail.com>
2021-01-12 00:40:15 +0530

    redis

--- src/main/java/org/anonymous/server/CachingTester.java
+++ src/main/java/org/anonymous/server/CachingTester.java
@@ -11,14 +11,15 @@ import redis.clients.jedis.JedisPoolConfig;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CachingTester {
 
     private static final ConnectionProviderHolder connectionProviderHolder = HikariCPConnectionProvider.create();
     public static final String AURORA_SIZING_UGA7QD_NG_0001_USE1_CACHE_AMAZONAWS_COM = "aurora-sizing.uga7qd.ng.0001.use1.cache.amazonaws.com";
     private static final Jedis jedis = new Jedis(AURORA_SIZING_UGA7QD_NG_0001_USE1_CACHE_AMAZONAWS_COM);
-    public static final int N_THREADS = 500;
     private static JedisPool jedisPool = null;
     private static final Thread shutdownHook = new Thread(() -> {
         connectionProviderHolder.close();
@@ -30,6 +31,10 @@ public class CachingTester {
     private static final Gauge getObjFromDBGaugeTimer = Gauge.build().name("get_object_db").help("Get Object on Middleware from db").labelNames("db").register();
     public static final String SEC_KEY = "232574-46439-1-1326302";
 
+
+    public static ExecutorService cacheService = null;
+    public static ExecutorService dbService = null;
+
     public static void main(String[] args) throws InterruptedException {
         Runtime.getRuntime().addShutdownHook(shutdownHook);
         GrpcServer.startMetricsServer();
@@ -37,24 +42,41 @@ public class CachingTester {
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMaxTotal(500);
         jedisPool = new JedisPool(poolConfig, AURORA_SIZING_UGA7QD_NG_0001_USE1_CACHE_AMAZONAWS_COM);
-        Executors.newFixedThreadPool(N_THREADS).execute(() -> {
+        boolean useCache = "true".equalsIgnoreCase(System.getProperty("testCache"));
+        if (useCache) {
+            System.out.println("start test with cache");
+            cacheService = Executors.newFixedThreadPool(Integer.MAX_VALUE);
+            int jobCount = 0;
+            int counter = 0;
             while (true) {
-                if (!SEC_KEY.equals(cachingTester.fromDB(SEC_KEY).get().name)) {
-                    System.out.println("error from db");
+                while (counter < jobCount) {
+                    cacheService.execute(() -> {
+                        if (!SEC_KEY.equals(cachingTester.fromCache(SEC_KEY).get().name)) {
+                            System.out.println("error from cache");
+                        }
+                    });
+                    counter++;
                 }
+                jobCount += 100;
+                counter = 0;
             }
-        });
-
-        Executors.newFixedThreadPool(1).execute(() -> {
+        } else {
+            System.out.println("start test with cache");
+            dbService = Executors.newFixedThreadPool(Integer.MAX_VALUE);
+            int jobCount = 0;
+            int counter = 0;
             while (true) {
-                if (!SEC_KEY.equals(cachingTester.fromCache(SEC_KEY).get().name)) {
-                    System.out.println("error from cache");
+                while (counter < jobCount) {
+                    dbService.execute(() -> {
+                        if (!SEC_KEY.equals(cachingTester.fromDB(SEC_KEY).get().name)) {
+                            System.out.println("error from db");
+                        }
+                    });
+                    counter++;
                 }
+                jobCount += 100;
+                counter = 0;
             }
-        });
-
-        while (true) {
-            Thread.sleep(Integer.MAX_VALUE);
         }
     }
 
