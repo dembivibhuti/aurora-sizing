04e7d05b10eecc7dfa499d9ba1dc9e3c30c28da7
somnath67643 <somnath67643@gmail.com>
2021-01-11 20:03:00 +0530

    redis

--- src/main/java/org/anonymous/server/CachingTester.java
+++ src/main/java/org/anonymous/server/CachingTester.java
@@ -6,6 +6,8 @@ import org.anonymous.connection.HikariCPConnectionProvider;
 import org.anonymous.domain.ObjectDTO;
 import org.anonymous.module.ObjectRepository;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -14,9 +16,15 @@ import java.util.concurrent.Executors;
 public class CachingTester {
 
     private static final ConnectionProviderHolder connectionProviderHolder = HikariCPConnectionProvider.create();
-    private static final Thread shutdownHook = new Thread(() -> connectionProviderHolder.close());
+    public static final String AURORA_SIZING_UGA7QD_NG_0001_USE1_CACHE_AMAZONAWS_COM = "aurora-sizing.uga7qd.ng.0001.use1.cache.amazonaws.com";
+    private static final Jedis jedis = new Jedis(AURORA_SIZING_UGA7QD_NG_0001_USE1_CACHE_AMAZONAWS_COM);
+    private static JedisPool jedisPool = null;
+    private static final Thread shutdownHook = new Thread(() -> {
+        connectionProviderHolder.close();
+        jedisPool.close();
+        jedis.close();
+    });
     private static final ObjectRepository objectRepository = new ObjectRepository(connectionProviderHolder.roConnectionProvider, connectionProviderHolder.rwConnectionProvider);
-    private static final Jedis jedis = new Jedis("aurora-sizing.uga7qd.ng.0001.use1.cache.amazonaws.com");
     private static final Gauge getObjFromCacheGaugeTimer = Gauge.build().name("get_object_cache").help("Get Object on Middleware from cache").labelNames("redis").register();
     private static final Gauge getObjFromDBGaugeTimer = Gauge.build().name("get_object_db").help("Get Object on Middleware from db").labelNames("db").register();
     public static final String SEC_KEY = "232574-46439-1-1326302";
@@ -25,17 +33,20 @@ public class CachingTester {
         Runtime.getRuntime().addShutdownHook(shutdownHook);
         GrpcServer.startMetricsServer();
         CachingTester cachingTester = new CachingTester();
-        Executors.newFixedThreadPool(500).execute(() -> {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(500);
+        jedisPool = new JedisPool(poolConfig, AURORA_SIZING_UGA7QD_NG_0001_USE1_CACHE_AMAZONAWS_COM);
+        Executors.newFixedThreadPool(1).execute(() -> {
             while (true) {
-                if(!SEC_KEY.equals(cachingTester.fromDB(SEC_KEY).get().name)){
+                if (!SEC_KEY.equals(cachingTester.fromDB(SEC_KEY).get().name)) {
                     System.out.println("error from db");
                 }
             }
         });
 
-        Executors.newFixedThreadPool(500).execute(() -> {
+        Executors.newFixedThreadPool(1).execute(() -> {
             while (true) {
-                if(!SEC_KEY.equals(cachingTester.fromCache(SEC_KEY).get().name)){
+                if (!SEC_KEY.equals(cachingTester.fromCache(SEC_KEY).get().name)) {
                     System.out.println("error from cache");
                 }
             }
@@ -48,7 +59,8 @@ public class CachingTester {
 
     private Optional<ObjectDTO> fromCache(String key) {
         Gauge.Timer timer = getObjFromCacheGaugeTimer.labels("get_object_cache").startTimer();
-        byte[] fromCache = jedis.get(key.getBytes());
+        Jedis jed = jedisPool.getResource();
+        byte[] fromCache = jed.get(key.getBytes());
         Optional<ObjectDTO> result = Optional.empty();
         if (null != fromCache) {
             try {
@@ -60,9 +72,10 @@ public class CachingTester {
         } else {
             result = fromDB(key);
             if (result.isPresent()) {
-                jedis.set(key.getBytes(), result.get().toBytes());
+                jed.set(key.getBytes(), result.get().toBytes());
             }
         }
+        jed.close();
         return result;
     }
 
