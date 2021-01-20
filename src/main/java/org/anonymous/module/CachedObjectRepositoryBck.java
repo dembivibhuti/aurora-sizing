2cb46141c06c6fe3366fbd055a6d525f520ac35e
somnath67643 <somnath67643@gmail.com>
2021-01-20 15:00:00 +0530

    Bring back Jedis

--- src/main/java/org/anonymous/module/CachedObjectRepositoryBck.java
+++ src/main/java/org/anonymous/module/CachedObjectRepositoryBck.java
@@ -12,16 +12,13 @@ import org.redisson.config.Config;
 import org.redisson.config.TransportMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.List;
 import java.util.Optional;
 
-public class CachedObjectRepository implements AutoCloseable {
+public class CachedObjectRepositoryBck implements AutoCloseable {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CachedObjectRepository.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CachedObjectRepositoryBck.class);
 
     private static final Gauge getObjFromCacheGaugeTimer = Gauge.build().name("get_object_cache").help("Get Object on Middleware from cache").labelNames("redis").register();
     private static final Gauge getObjFromDBGaugeTimer = Gauge.build().name("get_object_db").help("Get Object on Middleware from db").labelNames("db").register();
@@ -30,23 +27,22 @@ public class CachedObjectRepository implements AutoCloseable {
     private static final Counter dbOps = Counter.build().name("get_object_db_count").help("Count of GetObject from DB").labelNames("db").register();
     private static final Counter jedisConns = Counter.build().name("redis_connection_count").help("Count of Redis Connections").labelNames("redis").register();
     public static final int MAX_TOTAL = 5000;
-    public static final String OBJ_MAP = "objMap";
 
     private final ObjectRepository delegate;
-    private final JedisPool replicaPool;
+    /*private final JedisPool replicaPool;
     private final JedisPool primaryPool;
     private final ThreadLocal<Jedis> jedisROConnection;
-    private final ThreadLocal<Jedis> jedisRWConnection;
+    private final ThreadLocal<Jedis> jedisRWConnection;*/
 
-    /*private static RedissonClient redisson;
-    private static RLocalCachedMap<String, ObjectDTO> objMap;*/
+    private static RedissonClient redisson;
+    private static RLocalCachedMap<String, ObjectDTO> objMap;
 
-    public CachedObjectRepository(ObjectRepository objectRepository) {
+    public CachedObjectRepositoryBck(ObjectRepository objectRepository) {
         this.delegate = objectRepository;
         // max pool size and end point externalize
         // src/redis-cli -c -h aurora-sizing.uga7qd.ng.0001.use1.cache.amazonaws.com -p 6379
 
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        /*JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMaxTotal(MAX_TOTAL);
         poolConfig.setMaxWaitMillis(Integer.MAX_VALUE);
         this.primaryPool = new JedisPool(poolConfig, System.getProperty("redis.pri"));
@@ -58,9 +54,9 @@ public class CachedObjectRepository implements AutoCloseable {
         jedisRWConnection = ThreadLocal.withInitial(() -> {
             jedisConns.labels("conn count").inc();
             return primaryPool.getResource();
-        });
+        });*/
 
-        /*Config config = new Config();
+        Config config = new Config();
         config.setTransportMode(TransportMode.EPOLL).setNettyThreads(500);
         config.useReplicatedServers()
                 .addNodeAddress("redis://aurora-sizing-001.uga7qd.0001.use1.cache.amazonaws.com:6379")
@@ -72,14 +68,13 @@ public class CachedObjectRepository implements AutoCloseable {
                 .setMasterConnectionPoolSize(1000);
 
         redisson = Redisson.create(config);
-        objMap = redisson.getLocalCachedMap(OBJ_MAP, LocalCachedMapOptions.defaults());*/
+        objMap = redisson.getLocalCachedMap("objMap", LocalCachedMapOptions.defaults());
 
     }
 
     @Override
     public void close() throws Exception {
-        primaryPool.close();
-        replicaPool.close();
+        redisson.shutdown();
     }
 
     public List<String> lookup(String prefix, int typeid, int limit) {
@@ -112,7 +107,7 @@ public class CachedObjectRepository implements AutoCloseable {
     private void setToRedis(String key, Optional<ObjectDTO> objectDTO) {
         Gauge.Timer setCacheTimer = setObjToCacheGaugeTimer.labels("set_object_cache").startTimer();
         try {
-            jedisRWConnection.get().hset(OBJ_MAP.getBytes(), key.getBytes(), objectDTO.get().toBytes());
+            objMap.fastPut(key, objectDTO.get());
         } catch (Throwable th) {
             LOGGER.error("unexpected err in Redis set ", th);
         }
@@ -122,7 +117,7 @@ public class CachedObjectRepository implements AutoCloseable {
     private ObjectDTO getFromRedis(String key) {
         ObjectDTO objectDTO = null;
         try {
-            objectDTO = ObjectDTO.fromBytes(jedisROConnection.get().hget(OBJ_MAP.getBytes(), key.getBytes()));
+            objectDTO = objMap.get(key);
         } catch (Throwable th) {
             LOGGER.error("unexpected err in Redis get ", th);
         }
