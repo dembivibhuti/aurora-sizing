722b28ec09961b087abd53365999fc84d69fd81c
somnath67643 <somnath67643@gmail.com>
2021-01-21 20:33:39 +0530

    cache warm up

--- src/main/java/org/anonymous/module/NearCachedObjectRepository.java
+++ src/main/java/org/anonymous/module/NearCachedObjectRepository.java
@@ -171,6 +171,13 @@ public class NearCachedObjectRepository implements AutoCloseable {
     }
 
     private void warmupNearCache() {
+        String[] indexes = {"Table_BBI", "Table_BG", "Table_PB", "Table_PNT", "Table_TETID", "Table_TMID", "Table_TST",
+                "Table_TT", "Table_EBBI", "Table_MIMID"};
+        LOGGER.info("Populating Index Data ..... ");
+        Arrays.asList(indexes).parallelStream().forEach(index -> delegate.getIndexRecordMany("", index, 0).parallelStream().forEach(indexRecDTO ->
+                jedisRWConnection.get().hsetnx(index.getBytes(), indexRecDTO.objKey.getBytes(), indexRecDTO.toBytes())));
+        LOGGER.info("Done Populating Index Data");
+
         new Thread(() -> {
             long start = System.currentTimeMillis();
             int chunkSize = 100000;
@@ -188,13 +195,6 @@ public class NearCachedObjectRepository implements AutoCloseable {
                     System.out.print("Cache Warm Up | Remaining Keys " + remainingCount +  "(" + (((double)recCnt - (double)remainingCount) / (double)recCnt ) * 100 + "%)" + " Time = " + (System.currentTimeMillis() - start ) / (1000 * 60 )+ " mins \r");
                 });
             }
-
-            String[] indexes = {"Table_BBI", "Table_BG", "Table_PB", "Table_PNT", "Table_TETID", "Table_TMID", "Table_TST",
-                    "Table_TT", "Table_EBBI", "Table_MIMID"};
-            LOGGER.info("Populating Index Data ..... ");
-            Arrays.asList(indexes).parallelStream().forEach(index -> delegate.getIndexRecordMany("", index, 0).parallelStream().forEach(indexRecDTO ->
-                    jedisRWConnection.get().hsetnx(index.getBytes(), indexRecDTO.objKey.getBytes(), indexRecDTO.toBytes())));
-            LOGGER.info("Done Populating Index Data");
         }).start();
     }
 
