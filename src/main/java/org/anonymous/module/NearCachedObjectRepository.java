ad85829fc5566d4cd8688eeca03ba7b3dad84f02
somnath67643 <somnath67643@gmail.com>
2021-01-21 14:43:23 +0530

    cache warm up

--- src/main/java/org/anonymous/module/NearCachedObjectRepository.java
+++ src/main/java/org/anonymous/module/NearCachedObjectRepository.java
@@ -172,7 +172,7 @@ public class NearCachedObjectRepository implements AutoCloseable {
     }
 
     private void warmupNearCache() {
-        int chunkSize = 20000;
+        int chunkSize = 40000;
         ExecutorService warmupService = Executors.newFixedThreadPool(32);
 
         long recCnt = delegate.countRecs();
