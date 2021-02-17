4a4c9d420e850c7f2e4e5412b0c7dcfa40617d5e
rahulbindu <rahul.bindu@gmail.com>
2021-02-17 17:55:19 +0530

    Use prepared statements

--- middleware-cpp/server/db/repository.h
+++ middleware-cpp/server/db/repository.h
@@ -15,53 +15,47 @@ static const char *const SELECT_OBJ = "select name, typeId, lastTransaction, tim
 
 static const char *const END_QUOTE = "'";
 
-namespace tao::pq
-{
+namespace tao::pq {
     class bytea  // NOLINT
     {
     private:
         std::size_t m_size;
-        unsigned char* m_data;
+        unsigned char *m_data;
 
     public:
-        explicit bytea( const char* value )
-                : m_size( 0 ), m_data( PQunescapeBytea( (unsigned char*)value, &m_size ) )  //NOLINT
+        explicit bytea(const char *value)
+                : m_size(0), m_data(PQunescapeBytea((unsigned char *) value, &m_size))  //NOLINT
         {
-            if( m_data == nullptr ) {
+            if (m_data == nullptr) {
                 throw std::bad_alloc();  // LCOV_EXCL_LINE
             }
         }
 
-        ~bytea()
-        {
-            PQfreemem( m_data );
+        ~bytea() {
+            PQfreemem(m_data);
         }
 
-        bytea( const bytea& ) = delete;
-        auto operator=( const bytea& ) -> bytea& = delete;
+        bytea(const bytea &) = delete;
 
-        [[nodiscard]] auto size() const noexcept
-        {
+        auto operator=(const bytea &) -> bytea & = delete;
+
+        [[nodiscard]] auto size() const noexcept {
             return m_size;
         }
 
-        [[nodiscard]] auto data() const noexcept -> const unsigned char*
-        {
+        [[nodiscard]] auto data() const noexcept -> const unsigned char * {
             return m_data;
         }
 
-        [[nodiscard]] auto operator[]( const std::size_t idx ) const noexcept -> unsigned char
-        {
-            return m_data[ idx ];
+        [[nodiscard]] auto operator[](const std::size_t idx) const noexcept -> unsigned char {
+            return m_data[idx];
         }
     };
 
     template<>
-    struct result_traits< bytea >
-    {
-        [[nodiscard]] static auto from( const char* value )
-        {
-            return bytea( value );
+    struct result_traits<bytea> {
+        [[nodiscard]] static auto from(const char *value) {
+            return bytea(value);
         }
     };
 
@@ -70,18 +64,28 @@ namespace tao::pq
 class Repository {
 public:
     Security *get_security(std::string &name, Gauge *gauge) {
+        thread_local bool connection_created = false;
         auto start = std::chrono::steady_clock::now();
 
         const auto conn = pool->connection();
-
         auto end = std::chrono::steady_clock::now();
         const long count = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
         gauge->dbConnection.Set(count);
 
         start = std::chrono::steady_clock::now();
+        try {
+            if (!connection_created) {
+                conn->prepare("get_sec",
+                              "select name, typeId, lastTransaction, timeUpdated, updateCount, dateCreated, dbIdUpdated, versionInfo, mem from objects where nameLower = $1");
+                connection_created = true;
+            }
+        }
+        catch (std::exception &ex) {
+            std::cerr << ex.what() << std::endl;
+        }
 
-        const auto rs = conn->execute(SELECT_OBJ + name + END_QUOTE);
-        if(rs.size() > 0) {
+        const auto rs = conn->execute("get_sec", name);
+        if (rs.size() > 0) {
             auto row = rs.at(0);
             Security *security = new Security;
             security->name = row[0].as<std::string>();
@@ -99,22 +103,32 @@ public:
             end = std::chrono::steady_clock::now();
             const long count1 = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
             gauge->queryExec.Set(count1);
-
             return security;
         }
-
 
         return nullptr;
     }
 
-    std::vector<std::string>* lookup(std::string &prefix, short &count) {
+    std::vector<std::string> *lookup(std::string &prefix, short &count) {
+        thread_local bool connection_created = false;
         std::vector<std::string> *objects = new std::vector<std::string>();
         const auto conn = pool->connection();
-        auto query = "select name from objects where nameLower >= '" + prefix +"' order by nameLower LIMIT " + std::to_string(count);
+        try {
+            if (!connection_created) {
+                conn->prepare("lookup",
+                              "select name from objects where nameLower >= $1 order by nameLower LIMIT $2");
+                connection_created = true;
+            }
+        }
+        catch (std::exception &ex) {
+            std::cerr << ex.what() << std::endl;
+        }
+        /*auto query = "select name from objects where nameLower >= '" + prefix + "' order by nameLower LIMIT " +
+                     std::to_string(count);*/
         //const auto rs = pool->execute(query);
-        const auto rs = conn->execute(query);
-        for( const auto& row : rs ) {
-            objects->emplace_back(row[0].as< std::string >());
+        const auto rs = conn->execute("lookup",prefix,count);
+        for (const auto &row : rs) {
+            objects->emplace_back(row[0].as<std::string>());
         }
         return objects;
     }
@@ -124,11 +138,13 @@ public:
 private:
 
     Repository() {
-        pool = tao::pq::connection_pool::create("postgresql://postgres:postgres@database-1.cluster-cpw6mwbci5yo.us-east-1.rds.amazonaws.com:5432/postgres");
+        pool = tao::pq::connection_pool::create(
+                "postgresql://postgres:postgres@database-1.cluster-cpw6mwbci5yo.us-east-1.rds.amazonaws.com:5432/postgres");
         //pool = tao::pq::connection_pool::create("dbname=rahul");
-        const auto conn = pool->connection();
-        conn->prepare("get_sec",
-                      "select name, typeId, lastTransaction, timeUpdated, updateCount, dateCreated, dbIdUpdated, versionInfo, length(mem), mem from objects where nameLower = $1");
+        /* const auto conn = pool->connection();
+         conn->prepare("get_sec",
+                       "select name, typeId, lastTransaction, timeUpdated, updateCount, dateCreated, dbIdUpdated, versionInfo, mem from objects where nameLower = $1");
+ */
     }
 
     ConnectionPool pool;
